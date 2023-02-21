ae21fdc213fe814a980846cb888b6fe7bd391b68
steven cornwall <taylorchangordie@icloud.com>
2023-02-21 00:52:07 -0600

    added review, reviewimage migrations  and models

---
+++ backend/db/migrations/20230221064226-create-review.js
@@ -0,0 +1,48 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      spotId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'Spots'
+        }
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'Users'
+        }
+      },
+      review: {
+        type: Sequelize.STRING
+      },
+      stars: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Reviews');
+  }
+};
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -39,6 +39,9 @@ module.exports = (sequelize, DataTypes) => {
     }
     static associate(models) {
       // define association here
+      User.hasMany(models.Review, {
+        foreignKey: 'userId'
+      })
     }
   };
 
