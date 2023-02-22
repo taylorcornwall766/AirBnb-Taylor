9b86b0f5b3dc4219b9b4065d656b05ae43c1ef46
steven cornwall <taylorchangordie@icloud.com>
2023-02-22 15:18:34 -0600

    fixed typo lowercase S on table.name in spot image seeder

--- backend/db/seeders/6-demo-spot-image.js
+++ backend/db/seeders/6-demo-spot-image.js
@@ -5,7 +5,7 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
-options.tableName = 'spotImages';
+options.tableName = 'SpotImages';
 module.exports = {
   async up (queryInterface, Sequelize) {
     return queryInterface.bulkInsert(options, [
