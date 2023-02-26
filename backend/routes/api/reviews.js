5c27292d9f7b63e0764a0dfe302ccdeb5c8dfed3
steven cornwall <taylorchangordie@icloud.com>
2023-02-26 02:56:02 -0600

    DELETE review completed

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -137,6 +137,11 @@ router.delete('/:reviewId', requireAuth, restoreUser, async(req, res) =>{
             statusCode: 403
         })
     }
+    await Review.destroy({where:{id: req.params.reviewId}})
+    return res.status(200).json({
+        message: "Successfully deleted",
+        statusCode: 200
+    })
 })
 
 module.exports = router
