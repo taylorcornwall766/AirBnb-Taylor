3fb7ae35dfc85b405f7fe6aa0b9e38d363304f4d
steven cornwall <taylorchangordie@icloud.com>
2023-02-27 11:49:46 -0600

    fixed get SESSION

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,6 +1,6 @@
 // backend/routes/api/session.js
 const express = require("express");
-const { setTokenCookie, restoreUser } = require("../../utils/auth");
+const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
@@ -38,7 +38,7 @@ router.delete("/", (_req, res) => {
   res.clearCookie("token");
   return res.json({ message: "success" });
 });
-router.get('/',restoreUser,(req, res) => {
+router.get('/',restoreUser, requireAuth,(req, res) => {
       const { user } = req;
       if (user) {
         return res.json({
