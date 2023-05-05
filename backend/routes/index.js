7dc5c238a5c3917d58dcf4df12486d20c73e9ac4
steven cornwall <taylorchangordie@icloud.com>
2023-05-05 10:51:38 -0500

    some change

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,40 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 router.use((req, res, next) => {
-   
+
    next()
 })
 
 router.use('/api', apiRouter)
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+   const path = require('path');
+   // Serve the frontend's index.html file at the root route
+   router.get('/', (req, res) => {
+     res.cookie('XSRF-TOKEN', req.csrfToken());
+     return res.sendFile(
+       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+     );
+   });
+
+   // Serve the static assets in the frontend's build folder
+   router.use(express.static(path.resolve("../frontend/build")));
+
+   // Serve the frontend's index.html file at all other routes NOT starting with /api
+   router.get(/^(?!\/?api).*/, (req, res) => {
+     res.cookie('XSRF-TOKEN', req.csrfToken());
+     return res.sendFile(
+       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+     );
+   });
+ }
+ if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
 // go to this url to get that XSRF
 // token for your fetch request:
 // http://localhost:8000/api/csrf/restore
