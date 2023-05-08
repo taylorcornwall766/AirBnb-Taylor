67b2a063f790a62370b4303b8c879591faf28128
steven cornwall <taylorchangordie@icloud.com>
2023-05-08 16:18:57 -0500

    get all spots thunk working

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -2,16 +2,35 @@ import { csrfFetch } from "./csrf";
 
 const GET_ALL_SPOTS = "spots/getAllSpots"
 
-const getAllSpots 
+const getAllSpots = (spots) => {
+    return {
+        type: GET_ALL_SPOTS,
+        payload: spots
+    }
+}
 
-const initialState = { spots: null };
+export const loadSpotsThunk = () => async(dispatch) =>{
+    const response = await fetch("/api/spots")
+    const data = await response.json()
+    // console.log(data.spots)
+    const normalSpots = {}
+    data.spots.forEach((spot) => {
+        normalSpots[spot.id] = spot
+    })
+    dispatch(getAllSpots(normalSpots))
+    return normalSpots
+}
+
+const initialState = {allSpots: null, singleSpot: null};
 
 const spotsReducer = (state = initialState, action) => {
-  let newState;
   switch (action.type) {
-    case SET_SPOTS:
-
-      return newState;
+    case GET_ALL_SPOTS:{
+        const newState = {...state}
+        // console.log('1',newState)
+        newState.allSpots = {...action.payload}
+        return newState;
+    }
     default:
       return state;
   }
