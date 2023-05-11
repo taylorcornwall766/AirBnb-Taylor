9661f809e256827e8273383789e11c97bcf490a1
steven cornwall <taylorchangordie@icloud.com>
2023-05-11 13:06:24 -0500

    form works!

--- frontend/src/components/CreateSpotForm/index.js
+++ frontend/src/components/CreateSpotForm/index.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 // import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
+import { createSpotThunk } from "../../store/spots";
 
 function CreateSpotForm(){
     const dispatch = useDispatch()
 
-    const [country, setCountry] = useState("United States")
-    const [streetAddress, setStreetAddress] = useState('')
+    const [country, setCountry] = useState("")
+    const [streetAddress, setStreetAddress] = useState("")
     const [city, setCity] = useState("")
     const [state, setState] = useState("")
     const [latitude, setLatitude] = useState("")
@@ -19,32 +20,72 @@ function CreateSpotForm(){
     const [image2Url, setImage2Url] = useState("")
     const [image3Url, setImage3Url] = useState("")
     const [image4Url, setImage4Url] = useState("")
-
-    const errors = {}
+    const [errors, setErrors] = useState({})
+    // const errors = {}
     const handleSubmit = (e) => {
+        // console.log("previewImageUrl: ", previewImageUrl)
+        // console.log("previewImageUrl.length: ", previewImageUrl.length)
+        setErrors({})
+        console.log('asdf')
         e.preventDefault()
-
+        console.log(!country.length)
         if(!country.length){
-            errors.country = "Country is required"
+            setErrors({...errors, country : (<p className="errors" value="Country is required">Country is required</p>)})
         }
-        if(!address.length){
-            errors.address = "Address is required"
+        console.log("1 :", errors)
+        if(!streetAddress.length){
+            setErrors({...errors, streetAddress: (<p className="errors" value="Address is required">Address is required</p>)})
         }
+        console.log("2 :", errors)
         if(!city.length){
-            errors.city = "City is required"
+            setErrors({...errors,city : <p className="errors" value="City is required">City is required</p>})
         }
+        console.log("3 :", errors)
         if(!state.length){
-            errors.state = "State is required"
+            setErrors({...errors, state:(<p className="errors" value="State is required">State is required</p>)})
         }
         if(description.length < 30){
-            errors.description = "Description needs a minimum of 30 characters"
+            setErrors({...errors, description:(<p className="errors" value="Description needs a minimum of 30 characters">Description needs a minimum of 30 characters</p>)})
         }
         if(!name.length){
-            errors.name = "Name is required"
+            setErrors({...errors,name : (<p className="errors" value="Name is required">Name is required</p>)})
+        }
+        if(!price.length){
+            setErrors({...errors,price : (<p className="errors" value="Price is required">Price is required</p>)})
+        }
+        if(!previewImageUrl.length){
+            setErrors({...errors, previewImageUrl: (<p className="errors" value="Preview image is required">Preview image is required</p>)})
         }
 
-    }
+        if(!Object.values(errors).length){
 
+        }
+        const newSpot = {
+            address: streetAddress,
+            city: city,
+            state: state,
+            country: country,
+            name: name,
+            description: description,
+            price: Number(price),
+            lat: 0,
+            lng: 0,
+        }
+        if(latitude.length){
+            newSpot.lat = Number(latitude)
+        }
+        if(longitude.length){
+            newSpot.lng = Number(longitude)
+        }
+        const spotImages = []
+        console.log(newSpot)
+        console.log("preview image url: ", previewImageUrl)
+        if(!Object.values(errors).length){
+            dispatch(createSpotThunk(newSpot, spotImages))
+        }
+        console.log('errors:   ',errors)
+        // if(image1Url && image1Url.split('.')[image1Url.split('.').length-1])
+    }
     return (
         <>
             <h1>Create a New Spot</h1>
@@ -57,8 +98,8 @@ function CreateSpotForm(){
                     type="text"
                     value={country}
                     onChange={(e) => setCountry(e.target.value)}
-                    required
                     />
+                    {errors.country}
                 </label>
                 <label>
                     Street Address
@@ -66,8 +107,8 @@ function CreateSpotForm(){
                     type="text"
                     value={streetAddress}
                     onChange={(e) => setStreetAddress(e.target.value)}
-                    required
                     />
+                    {errors.streetAddress}
                 </label>
                 <label>
                     City
@@ -75,8 +116,8 @@ function CreateSpotForm(){
                     type="text"
                     value={city}
                     onChange={(e) => setCity(e.target.value)}
-                    required
                     />
+                    {errors.city}
                 </label>
                 <label>
                     State
@@ -84,8 +125,8 @@ function CreateSpotForm(){
                     type="text"
                     value={state}
                     onChange={(e) => setState(e.target.value)}
-                    required
                     />
+                    {errors.state}
                 </label>
                 <label>
                     Latitude
@@ -108,12 +149,12 @@ function CreateSpotForm(){
                     <p>
                         Mention the best features of your space, any special amenities like fast wifi or parking, and what you love about the neighborhood.
                     </p>
-                    <input
+                    <textarea
                     type="text"
                     value={description}
                     onChange={(e) => setDescription(e.target.value)}
-                    required
                     />
+                    {errors.description}
                 </label>
                 <label>
                     <h3>Create a title for your spot</h3>
@@ -124,8 +165,8 @@ function CreateSpotForm(){
                     type="text"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
-                    required
                     />
+                    {errors.name}
                 </label>
                 <label>
                     <h3>Set a base price for your spot</h3>
@@ -137,8 +178,8 @@ function CreateSpotForm(){
                     type="number"
                     value={price}
                     onChange={(e) => setPrice(e.target.value)}
-                    required
                     />
+                    {errors.price}
                 </label>
                 <label>
                     <h3>Liven up your spot with photos</h3>
@@ -150,31 +191,27 @@ function CreateSpotForm(){
                     type="text"
                     value={previewImageUrl}
                     onChange={(e) => setPreviewImageUrl(e.target.value)}
-                    required
                     />
+                    {errors.previewImageUrl}
                     <input
                     type="text"
                     value={image1Url}
                     onChange={(e) => setImage1Url(e.target.value)}
-                    required
                     />
                     <input
                     type="text"
                     value={image2Url}
                     onChange={(e) => setImage2Url(e.target.value)}
-                    required
                     />
                     <input
                     type="text"
                     value={image3Url}
                     onChange={(e) => setImage3Url(e.target.value)}
-                    required
                     />
                     <input
                     type="text"
                     value={image4Url}
                     onChange={(e) => setImage4Url(e.target.value)}
-                    required
                     />
                 </label>
 
--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -2,6 +2,17 @@ import { csrfFetch } from "./csrf";
 
 const GET_ALL_SPOTS = "spots/getAllSpots"
 const GET_SPOT_DETAILS = "spots/getSpotDetails"
+const POST_NEW_SPOT = "spots/createNewSpot"
+const DELETE_SPOT = "spots/deleteSpot"
+const EDIT_SPOT = "spots/editSpot"
+const GET_USER_SPOTS = "spots/getUserSpots"
+
+const getUserSpots = (spots) => {
+    return {
+        type: GET_USER_SPOTS,
+        payload: spots
+    }
+}
 
 const getAllSpots = (spots) => {
     return {
@@ -10,11 +21,38 @@ const getAllSpots = (spots) => {
     }
 }
 
+// const editSpot = (spot) => {
+//     return {
+//         type: EDIT_SPOT,
+//         payload: spot
+//     }
+// }
+
+const deleteSpot = (spotId) => {
+    return {
+        type: DELETE_SPOT,
+        payload: spotId
+    }
+}
+
 const getSpotDetails = (spot) => {
     return {
         type: GET_SPOT_DETAILS,
         payload: spot
     }
+}
+
+const createNewSpot = (spot) => {
+    return {
+        type: POST_NEW_SPOT,
+        payload:{
+            spot
+        }
+    }
+}
+
+export const loadUserSpotsThunk = (userId) => async(dispatch) =>{
+    const response = await csrfFetch(`/api/spots/current`)
 }
 
 export const loadSpotDetailsThunk = (spotId) => async(dispatch) =>{
@@ -43,6 +81,68 @@ export const loadSpotsThunk = () => async(dispatch) =>{
     return null
 }
 
+export const createSpotThunk = (spot, spotImages) => async(dispatch) =>{
+    console.log(spot)
+    console.log(spotImages)
+    const response = await csrfFetch(`/api/spots`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(spot),
+      });
+    if(response.ok){
+        const spotData = await response.json()
+        console.log("spotData",spotData)
+        console.log("message", spotData.errors)
+        for(let i = 0; i < spotImages.length; i++){
+            const response = await csrfFetch(`/api/spots/${spotData.id}/images`, {
+                method: "post",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(spotImages[i]),
+              });
+            const imageData = await response.json()
+            console.log(imageData)
+        }
+        dispatch(createNewSpot(spotData))
+    }else{
+        const data = await response.json()
+        console.log(data.message)
+    }
+}
+
+export const deleteSpotThunk = (spotId) => async(dispatch) => {
+    const response = await csrfFetch(`/api/spots/${spotId}`, {
+        method: "delete",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    const data = await response.json()
+    if(response.ok){
+        console.log("deleted")
+        dispatch(deleteSpot(spotId))
+        return data
+    }
+    return data
+}
+
+export const editSpotThunk = (spot) => async(dispatch) =>{
+    const response = await csrfFetch(`/api/spots/${spot.id}`, {
+        method: "put",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(spot)
+      });
+    const data = await response.json()
+    if(response.ok){
+        dispatch(createNewSpot(spot))
+    }
+}
+
 const initialState = {allSpots: null, singleSpot: null};
 
 const spotsReducer = (state = initialState, action) => {
@@ -56,6 +156,17 @@ const spotsReducer = (state = initialState, action) => {
     case GET_SPOT_DETAILS:{
         const newState = {...state}
         newState.singleSpot = {...action.payload}
+        return newState
+    }
+    case DELETE_SPOT:{
+        const newState = {...state}
+        delete newState.allSpots[action.payload]
+        return newState
+    }
+    case POST_NEW_SPOT:{
+        const newState = {...state}
+        console.log(action.playload)
+        newState.allSpots[action.payload.id] = action.payload
         return newState
     }
     default:
